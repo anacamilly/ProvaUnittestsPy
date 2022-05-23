b12bd578e22011d26d28e9ba8cf97c67706f7510
Ana Camilly Guedes <90116902+anacamilly@users.noreply.github.com>
2022-05-23 11:00:24 -0300

    fifth commit

--- test/listaValoresVendaMensal4_test.py
+++ test/listaValoresVendaMensal4_test.py
@@ -1,13 +1,11 @@
 from listaValoresVendaMensal import *
 import unittest
+
 class test_total_produtos(unittest.TestCase):
-    def test_percentual_lista_produtos(self):
-        venda_mensal = [('1/22', 400.50), ('2/22', 1000.00), ('3/22', 10.50), ('4/22', 100.30)]
+    def test_melhoria_vendas_produtos(self):
+        situacao1 = [('1/22', 400.50), ('2/22', 1000.00), ('3/22', 10.50), ('4/22', 10.50)]
 
-        situacao1 = [('1/22', 400.50), ('2/22', 1000.00)]
-        r_esperado1 = 149.0
-        situacao2 = [('2/22', 1000.00), ('3/22', 10.50)]
-        r_esperado2 = -98.95
-
-        self.assertEqual(valores_venda_mensal(situacao1), r_esperado1)
-        self.assertEqual(valores_venda_mensal(situacao2), r_esperado2)
+        self.assertEqual(melhoria_vendas('1/22', situacao1), False)
+        self.assertEqual(melhoria_vendas('2/22', situacao1), 1.4969)
+        self.assertEqual(melhoria_vendas('3/22', situacao1), -0.9895)
+        self.assertEqual(melhoria_vendas('4/22', situacao1), 0)
